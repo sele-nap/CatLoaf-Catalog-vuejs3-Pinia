0e9d21f7f539062720eeb426eaabc6c4f850075c
Séléna <selena.poun@gmail.com>
2024-06-27 15:35:19 +0200

    add components

---
+++ src/components/CatList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <h2>Cat List</h2>
+      <ul>
+        <li v-for="cat in cats" :key="cat.id">
+          {{ cat.name }}
+          <button @click="removeCat(cat.id)">Remove</button>
+        </li>
+      </ul>
+      <input v-model="newCatName" placeholder="Add a new cat" />
+      <button @click="addNewCat">Add Cat</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import { useCatStore } from '../stores/catStore';
+  
+  const catStore = useCatStore();
+  const { cats, addCat, removeCat } = catStore;
+  
+  const newCatName = ref('');
+  
+  const addNewCat = () => {
+    if (newCatName.value.trim()) {
+      addCat(newCatName.value);
+      newCatName.value = '';
+    }
+  };
+  </script>
+  
