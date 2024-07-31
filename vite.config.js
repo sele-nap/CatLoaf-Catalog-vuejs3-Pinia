438f41f6abd9a61374d338a5780e2f7665740a0e
Séléna <selena.poun@gmail.com>
2024-07-31 17:48:03 +0200

    weird thing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,4 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: "https://sele-nap.github.io/CatLoaf-Catalog-vuejs3-Pinia/",
-  assetsInclude: ["**/*.JPG"],
-  base: "./"
 });
